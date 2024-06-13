e1a8196c568bfee5b5d620d8c65dda39b0c0ebe8
Maxime <maxime.loir@lepoles.org>
2024-06-13 16:42:44 +0200

    13/06/24

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Boutique</title>
+    <link rel="stylesheet" href="public/assets/css/boutique.css">
+</head>
+<body>
+    <div class="header">
+        <div class="logo">
+            <img src="public/assets/img/logo.png" alt="Ma Boutique Logo">
+        </div>
+            <div class="logo-text">
+                <h1>Ma Boutique</h1>
+                <p>Vente en ligne. Tendance et Déco.</p>
+            </div>
+        </div>
+        <div class="help">
+            <p>Besoin d'aide?</p>
+            <div class="help-contact">
+                <img src="public/assets/img/aide.png" alt="Phone">
+                <p>0800 10 28 10</p>
+            </div>
+            <p>Mon compte</p>
+            <div class="account-info">
+                <img src="public/assets/img/article.png" alt="Cart">
+                <p>0 article(s) dans votre panier.</p>
+            </div>
+    <div class="navbar">
+        <a href="#">Accueil</a>
+        <a href="#">Vetements</a>
+        <a href="#">Maison</a>
+        <a href="#">Technologie</a>
+        <a href="#">Bijoux</a>
+        <a href="#">Idees Cadeaux</a>
+    </div>
+    <div class="main-content">
+        <div class="ad-banner">
+            <img src="public/assets/img/bandeau.png" alt="Azzaro Pour Homme">
+            <p>Enrique Iglesias</p>
+        </div>
+        <div class="info-section">
+            <div class="secure-payment">
+                <h2>Paiement Sécurisé</h2>
+                <p>Réglez vos achats en toute sérénité sur maboutique.com</p>
+                <div class="payment-icons">
+                    <img src="public/assets/img/paiement.png" alt="Visa">
+                </div>
+            </div>
+            <div class="about-us">
+                <h2>Qui sommes nous?</h2>
+                <p>Bienvenue chez MaBoutique! MaBoutique est la plus grande boutique en ligne de France consacrée à la mode et à la beauté pour femmes et hommes. Nous proposons plus de 850 marques ainsi que nos propres collections. N'hésitez pas les livraisons et les retours sont gratuits. Peut-être possible de votre commande dans notre entrepot dus RDV.</p>
+            </div>
+            <div class="services">
+                <h2>Services</h2>
+                <p>Livraison sous 48h</p>
+                 <div class="truck-icons">
+                    <img src="public/assets/img/camion.png" alt="Camion">
+                </div>
+                <p>Produits Echangeables</p>
+                                 <div class="echange-icons">
+                    <img src="public/assets/img/echange.png" alt="Echange">
+                </div>
+            </div>
+        </div>
+        <div class="product-section">
+            <div class="product-block">
+                <h2>Bon Plan</h2>
+                <img src="public/assets/img/robe.png" alt="Product 1">
+                <p>bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc</p>
+                <a href="#">Voir</a>
+            </div>
+            <div class="product-block">
+                <h2>Coup de coeur</h2>
+                <img src="public/assets/img/talons.png" alt="Product 2">
+                <p>bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc</p>
+                <a href="#">Voir</a>
+            </div>
+            <div class="product-block">
+                <h2>Objectif Plaisir</h2>
+                <img src="public/assets/img/chemise.png" alt="Product 3">
+                <p>bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc bloc</p>
+                <a href="#">Voir</a>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <a href="#">Newsletter</a>
+        <a href="#">Promotions</a>
+        <a href="#">Nouveaux produits</a>
+        <a href="#">Meilleures ventes</a>
+        <a href="#">Mentions légales</a>
+        <a href="#">Conditions Générales de Vente</a>
+        <a href="#">Contactez-nous</a>
+    </div>
+</body>
+</html>
